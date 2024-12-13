c0a43ae0ec04d7d3fac0a396920fbff726c4e6fc
Belphin <dmitrylukienko.work@gmail.com>
2024-12-14 00:40:53 +0200

    Add animations

--- src/components/quiz-form/quiz-tree.jsx
+++ src/components/quiz-form/quiz-tree.jsx
@@ -1,21 +1,107 @@
 import { faculties } from "@/constants/quiz";
 import { Box } from "@chakra-ui/react";
+import { motion } from "framer-motion";
 import React, { useState } from "react";
+
+const MotionBox = motion(Box);
+
+const PingQuestionMark = ({ ping }) => {
+  return (
+    <motion.div
+      style={{
+        display: "inline-block",
+        textAlign: "center",
+      }}
+      animate={ping ? { scale: [1, 1.6, 1] } : { scale: 1 }}
+      transition={{
+        duration: ping ? 2 : 0.5,
+        repeat: ping ? Infinity : 0,
+        repeatType: "loop",
+      }}
+    >
+      ?
+    </motion.div>
+  );
+};
+
+const CircularBorderBox = ({ active, angle, size = 68 }) => {
+  const radius = (size - 10) / 2;
+  const circumference = 2 * Math.PI * radius;
+
+  return (
+    active && (
+      <>
+        <motion.svg
+          style={{
+            position: "absolute",
+            top: (60 - size) / 2,
+            left: (60 - size) / 2,
+            transform: `rotate(${angle + 180}deg)`,
+          }}
+          width={`${size}px`}
+          height={`${size}px`}
+          viewBox={`0 0 ${size} ${size}`}
+          xmlns="http://www.w3.org/2000/svg"
+          initial={{ strokeDashoffset: circumference }}
+          animate={{
+            strokeDashoffset: active ? circumference / 2 : circumference,
+          }}
+          transition={{ duration: 1 }}
+        >
+          <circle
+            cx={size / 2}
+            cy={size / 2}
+            r={radius}
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeDasharray={circumference}
+          />
+        </motion.svg>
+
+        <motion.svg
+          style={{
+            position: "absolute",
+            top: (60 - size) / 2,
+            left: (60 - size) / 2,
+            transform: `rotate(${angle + 180}deg)`,
+          }}
+          width={`${size}px`}
+          height={`${size}px`}
+          viewBox={`0 0 ${size} ${size}`}
+          xmlns="http://www.w3.org/2000/svg"
+          initial={{ strokeDashoffset: circumference }}
+          animate={{
+            strokeDashoffset: active ? circumference * 1.5 : circumference,
+          }}
+          transition={{ duration: 1 }}
+        >
+          <circle
+            cx={size / 2}
+            cy={size / 2}
+            r={radius}
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeDasharray={circumference}
+          />
+        </motion.svg>
+      </>
+    )
+  );
+};
 
 const calculateAngle = (startAngle, index, itemsLength) => {
   let angle = startAngle + 40 * (index - Math.floor(itemsLength / 2));
   return angle < 0 ? 360 + angle : angle;
 };
 
-const CircleContainer = ({ children }) => (
+const CircleContainer = ({ children, ping }) => (
   <Box className="circle-container">
-    <Box
-      borderStyle={"solid"}
-      className="circle-item"
-      fontWeight="bold"
-      fontSize={24}
-    >
-      ?
+    <Box borderStyle={"solid"} className="circle-item-position">
+      <Box className="circle-item start-item">
+        <PingQuestionMark ping={ping} />
+      </Box>
     </Box>
     {children}
   </Box>
@@ -33,40 +119,69 @@ const CircleItem = ({
   lineHeightOffset,
   radiusOffset = 1,
 }) => (
-  <Box
-    onClick={onClick}
-    color={active ? undefined : "GrayText"}
-    borderStyle={active ? "solid" : undefined}
-    className="circle-item"
-    transform={`rotate(${angle}deg) translate(${
-      radius * radiusOffset
-    }px) rotate(-${angle}deg)`}
-    top={top}
-    left={left}
+  <MotionBox
+    initial={{ opacity: 0 }}
+    animate={{ opacity: 1 }}
+    exit={{ opacity: 0 }}
+    transition={{ duration: 0.5 }}
   >
-    <ConnectorLine
-      angle={angle}
-      active={active}
-      radius={radius}
-      heightOffset={lineHeightOffset}
-    />
-    {label}
-    {active && children}
-  </Box>
+    <Box
+      onClick={onClick}
+      className="circle-item-position"
+      transform={`rotate(${angle}deg) translate(${
+        radius * radiusOffset
+      }px) rotate(-${angle}deg)`}
+      top={top}
+      left={left}
+      color={active ? undefined : "GrayText"}
+    >
+      <CircularBorderBox angle={angle} active={active} />
+      <ConnectorLine
+        angle={angle}
+        active={active}
+        radius={radius}
+        heightOffset={lineHeightOffset}
+      />
+      <Box className="circle-item" style={{ borderColor: "GrayText" }}>
+        {label}
+      </Box>
+      {active && children}
+    </Box>
+  </MotionBox>
 );
 
-const ConnectorLine = ({ angle, active, heightOffset, radius }) => (
-  <Box
-    className="line"
-    borderStyle={active ? "solid" : undefined}
-    transform={`rotate(${angle + 90}deg)`}
-    style={{
-      height: `calc(${radius}px - ${heightOffset}px)`,
-    }}
-    top={`${50 + Math.sin((Math.PI / 180) * angle) * (radius - 160)}%`}
-    left={`${50 + Math.cos((Math.PI / 180) * angle) * (radius - 160)}%`}
-  />
-);
+const ConnectorLine = ({ angle, active, heightOffset, radius }) => {
+  const topPosition = 50 + Math.sin((Math.PI / 180) * angle) * (radius - 154);
+  const leftPosition = 50 + Math.cos((Math.PI / 180) * angle) * (radius - 154);
+  const lineHeight = `${radius - heightOffset}px`;
+
+  const boxStyle = {
+    height: lineHeight,
+    top: `${topPosition}%`,
+    left: `${leftPosition}%`,
+    transform: `rotate(${angle + 90}deg)`,
+    overflow: "hidden",
+  };
+
+  return (
+    <>
+      <Box className="line" borderColor={"GrayText"} style={boxStyle} />
+
+      {active && (
+        <MotionBox
+          borderStyle={"solid"}
+          className="line"
+          style={boxStyle}
+          initial={{ height: lineHeight }}
+          animate={{
+            height: 0,
+            transition: { duration: 1, ease: "easeOut" },
+          }}
+        />
+      )}
+    </>
+  );
+};
 
 const QuizTree = ({ radius }) => {
   const [activeFaculties, setActiveFaculties] = useState([]);
@@ -76,7 +191,7 @@ const QuizTree = ({ radius }) => {
   );
 
   return (
-    <CircleContainer>
+    <CircleContainer ping={!activeFaculties.length}>
       {faculties.map((faculty, index) => {
         const isFacultyActive = activeFaculties.includes(faculty.abbr);
         const facultyAngle = (360 / faculties.length) * index;
